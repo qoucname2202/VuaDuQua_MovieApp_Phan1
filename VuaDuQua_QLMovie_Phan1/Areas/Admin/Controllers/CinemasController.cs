a4971af4f21d5993927fbd42d8ce87670dd53fc5
DUONG NAM <duongquocnam224400@gmail.com>
2021-07-12 09:00:39 +0700

    update-MoviesAdminController

---
+++ VuaDuQua_QLMovie_Phan1/Areas/Admin/Controllers/CinemasController.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using VuaDuQua_QLMovie_Phan1.Models;
+using VuaDuQua_QLMovie_Phan1.Models.CinemaEntities;
+
+namespace VuaDuQua_QLMovie_Phan1.Areas.Admin.Controllers
+{
+    // Authorize xác thực đăng nhập
+    [Authorize]
+    public class CinemasController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Admin/Cinemas
+        public ActionResult Index()
+        {
+            return View(db.Cinemas.ToList());
+        }
+
+        // GET: Admin/Cinemas/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Cinema cinema = db.Cinemas.Find(id);
+            if (cinema == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cinema);
+        }
+
+        // GET: Admin/Cinemas/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Admin/Cinemas/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Name,Description")] Cinema cinema)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Cinemas.Add(cinema);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(cinema);
+        }
+
+        // GET: Admin/Cinemas/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Cinema cinema = db.Cinemas.Find(id);
+            if (cinema == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cinema);
+        }
+
+        // POST: Admin/Cinemas/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Name,Description")] Cinema cinema)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(cinema).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(cinema);
+        }
+
+        // GET: Admin/Cinemas/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Cinema cinema = db.Cinemas.Find(id);
+            if (cinema == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cinema);
+        }
+
+        // POST: Admin/Cinemas/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Cinema cinema = db.Cinemas.Find(id);
+            db.Cinemas.Remove(cinema);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
