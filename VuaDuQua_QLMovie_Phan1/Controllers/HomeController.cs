f5df8708bf7556deb117f62ab7c9aba6fa21b56e
DUONG NAM <duongquocnam224400@gmail.com>
2021-07-12 08:03:12 +0700

    Update-Changed_HomeController

--- VuaDuQua_QLMovie_Phan1/Controllers/HomeController.cs
+++ VuaDuQua_QLMovie_Phan1/Controllers/HomeController.cs
@@ -1,30 +1,40 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
+using VuaDuQua_QLMovie_Phan1.Models;
+using VuaDuQua_QLMovie_Phan1.Models.CinemaEntities;
 
 namespace VuaDuQua_QLMovie_Phan1.Controllers
 {
     public class HomeController : Controller
     {
+        private ApplicationDbContext db;
+
+        public HomeController()
+        {
+            db = new ApplicationDbContext();
+        }
+
         public ActionResult Index()
         {
-            return View();
+            return View(db.Movies.ToList());
         }
 
-        public ActionResult About()
+        public ActionResult Details(int? id)
         {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Movie movie = db.Movies.Find(id);
+            if (movie == null)
+            {
+                return HttpNotFound();
+            }
+            return View(movie);
         }
     }
 }
