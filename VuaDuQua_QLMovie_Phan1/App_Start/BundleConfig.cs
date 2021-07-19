fb6f374bc3b443f67f800b3a0acb28b947039b11
DUONG NAM <duongquocnam224400@gmail.com>
2021-07-19 09:11:50 +0700

    Update_Layout-MovieAndCss

--- VuaDuQua_QLMovie_Phan1/App_Start/BundleConfig.cs
+++ VuaDuQua_QLMovie_Phan1/App_Start/BundleConfig.cs
@@ -25,6 +25,25 @@ namespace VuaDuQua_QLMovie_Phan1
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            // Style Movie_App
+            bundles.Add(new ScriptBundle("~/bundles/script_movie").Include(
+                         "~/Scripts/js/jquery-2.1.4.min.js",
+                         "~/Scripts/js/jquery.magnific-popup.js",
+                         "~/Scripts/js/minicart.js",
+                         "~/Scripts/js/jquery-ui.js",
+                         "~/Scripts/js/jquery.flexisel.js",
+                         "~/Scripts/js/SmoothScroll.min.js",
+                         "~/Scripts/js/move-top.js",
+                         "~/Scripts/js/easing.js",
+                         "~/Scripts/js/bootstrap.js"));
+
+            bundles.Add(new StyleBundle("~/Content/movie_css").Include(
+                     "~/Content/css/bootstrap.css",
+                     "~/Content/css/style.css",
+                     "~/Content/css/font-awesome.css",
+                     "~/Content/css/popuo-box.css",
+                     "~/Content/css/jquery-ui1.css"));
         }
     }
 }
