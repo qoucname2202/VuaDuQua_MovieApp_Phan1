c8ad3593119e451e4a2bae76f9038105030b49c6
DUONG NAM <duongquocnam224400@gmail.com>
2021-07-19 10:56:02 +0700

    Complete-Details_Movies

--- VuaDuQua_QLMovie_Phan1/App_Start/BundleConfig.cs
+++ VuaDuQua_QLMovie_Phan1/App_Start/BundleConfig.cs
@@ -28,22 +28,27 @@ namespace VuaDuQua_QLMovie_Phan1
 
             // Style Movie_App
             bundles.Add(new ScriptBundle("~/bundles/script_movie").Include(
-                         "~/Scripts/js/jquery-2.1.4.min.js",
-                         "~/Scripts/js/jquery.magnific-popup.js",
-                         "~/Scripts/js/minicart.js",
-                         "~/Scripts/js/jquery-ui.js",
-                         "~/Scripts/js/jquery.flexisel.js",
-                         "~/Scripts/js/SmoothScroll.min.js",
-                         "~/Scripts/js/move-top.js",
-                         "~/Scripts/js/easing.js",
-                         "~/Scripts/js/bootstrap.js"));
+                        "~/Scripts/js/jquery-2.1.4.min.js",
+                        "~/Scripts/js/jquery.magnific-popup.js",
+                        "~/Scripts/js/minicart.js",
+                        "~/Scripts/js/jquery-ui.js",
+                        "~/Scripts/js/jquery.flexisel.js",
+                        "~/Scripts/js/SmoothScroll.min.js",
+                        "~/Scripts/js/move-top.js",
+                        "~/Scripts/js/easing.js",
+                        "~/Scripts/js/bootstrap.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/movie_detail").Include(
+                         "~/Scripts/js/imagezoom.js",
+                         "~/Scripts/js/jquery.flexslider.js"));
 
             bundles.Add(new StyleBundle("~/Content/movie_css").Include(
                      "~/Content/css/bootstrap.css",
                      "~/Content/css/style.css",
                      "~/Content/css/font-awesome.css",
                      "~/Content/css/popuo-box.css",
-                     "~/Content/css/jquery-ui1.css"));
+                     "~/Content/css/jquery-ui1.css",
+                     "~/Content/css/flexslider.css"));
         }
     }
 }
